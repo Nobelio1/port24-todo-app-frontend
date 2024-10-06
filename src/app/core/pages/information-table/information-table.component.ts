c7db6ba2f8a579306892dc61d38f4ca88a233fc9
Gino Alejandro <ginoalejandro2304@gmail.com>
2024-10-06 13:59:36 -0500

    Se implementa vista de informacion de la tabla

--- src/app/core/pages/information-table/information-table.component.ts
+++ src/app/core/pages/information-table/information-table.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-information-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './information-table.component.html',
   styles: ``
 })
 export class InformationTableComponent {
+
+  public stats = [1,2,3,4]
 
 }
